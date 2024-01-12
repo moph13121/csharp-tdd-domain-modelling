a385f285e6783efe1fe11b9365756ee3eef0847f
Philip Morud <philip.morud@no.experis.com>
2024-01-12 12:48:46 +0100

    Completed tests

--- tdd-domain-modelling.CSharp.Main/CohortManager.cs
+++ tdd-domain-modelling.CSharp.Main/CohortManager.cs
@@ -12,16 +12,32 @@ namespace tdd_domain_modelling.CSharp.Main
         
         public CohortManager() { }
 
-        public int Sum()
+        public double Sum()
         {
-
+            if (_cart.Count == 0) { return 0; } 
+            else
+            {
+                double total = 0;
+                foreach (var item in _cart)
+                {
+                    total += item.Item2 * item.Item3;
+                }
+                return total;
+            }
             return -1;
         }
 
         public bool AddItem(string name, double price, int count)
         {
-
+            try
+            {
+                _cart.Add((name, price, count));
+                return true;
+            }
+            catch
+            {
             return false;
+            }
         }
     }
 }
